51a3801c74450a18c21c5a03f480d8da414d8268
Nils Kriedner <nkriedner@googlemail.com>
2024-01-31 18:06:26 +0100

    Repair fixed position of hamburger icon

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log("connected...");
+// ELEMENT SELECTION
+const hamburgerIcon = document.querySelector(".hamburger");
+
+// EVENT LISTENER FOR THE HAMBURGER ICON
+hamburgerIcon.addEventListener("click", () => {
+    hamburgerIcon.classList.toggle("is-active");
+});
