6279e77c97ec47af41ca2bf351819e3dfcc4b971
Nils Kriedner <nkriedner@googlemail.com>
2024-01-31 20:09:00 +0100

    Create the slide navigation for mobile view

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 // ELEMENT SELECTION
 const hamburgerIcon = document.querySelector(".hamburger");
+const mainNav = document.querySelector(".header__nav--main");
+const navListItems = document.querySelectorAll(".header__nav--main li");
 
 // EVENT LISTENER FOR THE HAMBURGER ICON
 hamburgerIcon.addEventListener("click", () => {
     hamburgerIcon.classList.toggle("is-active");
+    mainNav.classList.toggle("is-active");
+});
+
+// EVENT LISTENER FOR THE NAV LINKS ->
+navListItems.forEach((navListItem) => {
+    navListItem.addEventListener("click", () => {
+        hamburgerIcon.classList.toggle("is-active");
+        mainNav.classList.toggle("is-active");
+    });
 });
