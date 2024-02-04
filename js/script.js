3e8315b179d7c88e318450f5d70e342075ab33e8
Nils Kriedner <nkriedner@googlemail.com>
2024-02-04 13:00:26 +0100

    Add animations to hero section & refactor css

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ let defaultBackground = 1;
 
 // ELEMENT SELECTION
 const hamburgerIcon = document.querySelector(".hamburger");
-const mainNav = document.querySelector(".header__nav--main");
-const navListItems = document.querySelectorAll(".header__nav--main li");
+const mainNav = document.querySelector(".header-navlist");
+const navListItems = document.querySelectorAll(".header-navlist li");
 const colorPaletteIcon = document.querySelector(".color-palette-icon");
 const colorPalettePreview = document.querySelector("#color-palette-preview");
-const headerHeroBgOverlay = document.querySelector(".header-hero-bg-overlay");
+const headerHeroBgOverlay = document.querySelector(".hero-bg-overlay");
 
 colorPalettePreview.style.background = "linear-gradient(to right, #24243e, #302b63, #0f0c29)";
 
@@ -37,39 +37,18 @@ colorPaletteIcon.addEventListener("click", () => {
         colorPalettePreview.style.background = "linear-gradient(to right, #8f94fb, #4e54c8)";
         headerHeroBgOverlay.style.background =
             "linear-gradient(to right, rgb(143, 148, 251, 0.9), rgb(78, 84, 200, 0.9))";
-        // only change nav background in mobile view
-        // if (screen.width < 768) {
-        //     mainNav.style.background = "linear-gradient(to right, #8f94fb, #4e54c8)";
-        // }
         defaultBackground = 2;
     } else if (defaultBackground === 2) {
         body.style.background = "linear-gradient(to right, #493240, #ff0099)";
         colorPalettePreview.style.background = "linear-gradient(to right, #493240, #ff0099)";
         headerHeroBgOverlay.style.background =
             "linear-gradient(to right, rgba(73, 50, 64, 0.9), rgb(255, 0, 153, 0.9))";
-        // only change nav background in mobile view
-        // if (screen.width < 768) {
-        //     mainNav.style.background = "linear-gradient(to right, #493240, #ff0099)";
-        // }
         defaultBackground = 3;
     } else if (defaultBackground === 3) {
         body.style.background = "linear-gradient(to right, #24243e, #302b63, #0f0c29)";
         colorPalettePreview.style.background = "linear-gradient(to right, #24243e, #302b63, #0f0c29)";
         headerHeroBgOverlay.style.background =
             "linear-gradient(to right, rgba(36, 36, 62, 0.9), rgba(48, 43, 99, 0.9), rgba(15, 12, 41, 0.9))";
-        // headerHeroBg.style.backgroundImage = `linear-gradient(to right, rgba(36, 36, 62, 0.9), rgba(48, 43, 99, 0.9), rgba(15, 12, 41, 0.9)),
-        // url("../img/coding-1.jpg")`;
-        // only change nav background in mobile view
-        // if (screen.width < 768) {
-        //     mainNav.style.background = "linear-gradient(to right, #24243e, #302b63, #0f0c29)";
-        // }
         defaultBackground = 1;
     }
 });
-
-// document.querySelector(".color-palette-icon").addEventListener("mouseover", () => {
-//     colorPalettePreview.style.display = "block";
-// });
-// document.querySelector(".color-palette-icon").addEventListener("mouseout", () => {
-//     colorPalettePreview.style.display = "none";
-// });
